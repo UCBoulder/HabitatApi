6df770bfb24cb914071598f1829122a1124defcb
Jack Lay <jack.lay@colorado.edu>
2023-10-12 17:58:29 -0600

    "Route Expansion"

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app  = require('express')();
+
 const PORT = 8080;
 
 app.listen(
@@ -6,7 +7,26 @@ app.listen(
 	() => console.log('alive on http://localhost:8080')
 )
 
+app.post('/APITEST', (req,res) => {
+	cosole.log(req.body)
+	res.status(200);
+	res.json({responseMessage: "MESSAGE RECEIVED"});
+});
+
+app.get('/APITEST', (req,res) => {
+	res.status(200).send({
+		message:"This is a POST ONLY route"
+	})
+})
+
+
 app.get('', (req,res) => {
+	res.status(200).send({
+		message:"Yo this the homepage get outta here"
+	})
+})
+
+app.get('/route1', (req,res) => {
 	res.status(200).send({
 		cows: '',
 		bogs: '',
@@ -14,8 +34,11 @@ app.get('', (req,res) => {
 	})
 })
 
+
+
 app.get('/secret', (req, res) => {
-	res.status(200).send({
-		ok:"you found it"
-	})
+  	res.status(200).send({
+  		ok:"you found it"
+  	})
 })
+
