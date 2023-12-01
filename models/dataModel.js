0eb01c1ba6b2fc48050d0550059753655f1e9c8f
Jack Lay <42285595+jackhlay@users.noreply.github.com>
2023-12-01 00:52:34 -0700

    Update dataModel.js
    Added an example of Ethan's object for reference

--- models/dataModel.js
+++ models/dataModel.js
@@ -14,6 +14,27 @@ const dataModel = {
         }
     },
 
+    // Ethan's object
+    // {
+    //     position: {
+    //       timestamp: 1697650532099,
+    //       mocked: false,
+    //       provider: 'fused',
+    //       coords: {
+    //         speed: 0,
+    //         heading: 0,
+    //         altitude: 0,
+    //         accuracy: 2000,
+    //         longitude: -122.08395287867832,
+    //         altitudeAccuracy: 0,
+    //         latitude: 37.42342342342342
+    //       }
+    //     }
+    //      UserID: 245jl2;k1j5r
+    //      Notes: "adslkfjasl;kjhf"
+    //      VerificationRating: Defualt = 1
+    //   }
+
     llTest: {
         UserID: Number,
         latitude: Number,
