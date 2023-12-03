8c9ca2bd161d0223a8e32a5f76cc1fbe6ee19510
JackLay1 <42285595+JackLay1@users.noreply.github.com>
2023-12-03 13:33:54 -0700

    Update insertObservation.ts

--- DB-files/DB-Operations/insertObservation.ts
+++ DB-files/DB-Operations/insertObservation.ts
@@ -3,14 +3,14 @@ import { Observation } from './interfaces';
 import { client } from '../index.js';
 
 //When called, this (insertobservation) should tahe the body fron ethan's request as a parameter, and within the function
-//, pick what we need from it, and fomm the observation (including adding post id, and other fields) and add it to the DB
+//pick what we need from it, and form the observation (including adding Observation id, user ID and other fields) and add it to the DB
 export const insertObservation = async (Observation: Observation) => {
     // Change insertObservations function parameter to take in the front end's json
-    // Add UserID, default to 0 for now.
-    // Using UUID (or other random method) add ObservationID
+    // Add/set UserID, default to 0 for now in Front End json or other variable.
+    // Using UUID (or other random method) add/set ObservationID in FE json or other variable.
     // From here he may be able to just upload the whole json object, or we can parse it into arguments for the dynamo command.
 
-    
+
   const params: PutItemCommandInput = {
     TableName: "Observations",
     Item: Observation as unknown as Record<string, AttributeValue>,
