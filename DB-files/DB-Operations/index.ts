9578cc591bf2a3f3dd3f37c3c7482153b83f759c
Joel Carlson <cjoeld2024@gmail.com>
2024-01-21 17:24:44 -0700

    API update

--- DB-files/DB-Operations/index.ts
+++
@@ -1,9 +0,0 @@
-// in DB-Operations/index-DB-Operations.mjs 
-
-export * from './createTable';
-export * from './insertObservation';
-export * from './viewAllObservations';
-export * from './batchUpload';
-export * from './interfaces';
-export * from './s3-Operations/test-Upload.js';
-
