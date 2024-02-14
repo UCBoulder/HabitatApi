91dc4b069c891564417637dc8ced6842ea736c88
Joel Carlson <cjoeld2024@gmail.com>
2024-02-14 11:21:37 -0700

    this is for the test message

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ const itemRoutes = require('./routes/routes');
 app.use('/', itemRoutes); // Use '/items' as the base path
 
 app.listen(PORT, () => console.log('Server is running on http://localhost:3000'));
+app.get('/test', (req, res) => {
+  res.json({ message: "This is a test message." });
+});
