2fbc35cfb0c933b572d20ae52de96ba658843450
Joel Carlson <cjoeld2024@gmail.com>
2024-02-27 09:11:54 -0700

    updated s3

--- DB-files/DB-Operations/s3-Operations/s3getObject.ts
+++ DB-files/DB-Operations/s3-Operations/s3getObject.ts
@@ -21,3 +21,38 @@ export async function s3GetPhoto_SignedURL(key: string) {
         return undefined;
     }
 }
+
+
+// import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+
+
+// const region = "us-east-1";
+// const client = new S3Client({ 
+//         region,
+//         credentials: {
+//             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//         },
+//         endpoint: process.env.CUSTOM_S3_ENDPOINT
+//     });
+
+// export async function s3GetPhoto_SignedURL(key: string) {
+//     //create the command to get the object
+//     const command = new GetObjectCommand({
+//         Bucket: "test-cow",
+//         Key: key,
+//     });
+
+//     try {
+//         //create a signed URL to get the object, only valid for a short amount of time
+//         return await getSignedUrl(client, command, { expiresIn: 60*60*2});//2 hours
+
+//     } catch (error) {
+//         console.error("Error getting object from S3:", error);
+//         return undefined;
+//     }
+// } 
--- DB-files/index.js
+++ DB-files/index.js
@@ -5,3 +5,12 @@ const client_dynamodb_1 = require("@aws-sdk/client-dynamodb");
 exports.client = new client_dynamodb_1.DynamoDBClient({
     region: "us-east-1", // For local DynamoDB, the region doesn't matter
 });
+// dotenv.config();
+// export const s3Client = new S3Client({
+//   region: "us-east-1",
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//   },
+//   endpoint: process.env.CUSTOM_S3_ENDPOINT
+// });
